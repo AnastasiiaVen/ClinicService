4d31dded2e59ac087ea6a18a608ecbb913e90f36
Anastasiia <venanastasiia@gmail.com>
2023-01-22 19:13:46 +0300

    Добавлены методы тестирования

--- ClinicService/Controllers/PetController.cs
+++ ClinicService/Controllers/PetController.cs
@@ -14,53 +14,80 @@ namespace ClinicService.Controllers
         private IPetRepository _petRepository;
         private object createRequest;
 
-        public PetController(IPetRepository petRepository)
+        /*public PetController(IPetRepository petRepository)
         {
             _petRepository = petRepository;
-        }
+        }*/
 
         [HttpPost("create")]
-        public ActionResult<int> Create([FromBody] CreatePetRequest createRequest)
+        public ActionResult<int> Create([FromBody] CreatePetRequest createPetRequest)
         {
-            int res = _petRepository.Create(new Pet
+            #region
+            /*int res = _petRepository.Create(new Pet
             {
-                ClientId = createRequest.ClientId,
-                Name = createRequest.Name,
-                Birthday = createRequest.Birthday,
+                ClientId = createPetRequest.ClientId,
+                Name = createPetRequest.Name,
+                Birthday = createPetRequest.Birthday,
             });
-            return Ok(res);
+            return Ok(res);*/
+            #endregion
+            if (createPetRequest.Birthday < DateTime.Now.AddYears(-25) || createPetRequest.Name.Length < 3)
+            {
+                return BadRequest(0); // BadRequestObjectResult : 400
+            }
+            return Ok(1); // OkObjectResult : 200
         }
 
         [HttpPut("update")]
         public ActionResult<int> Update([FromBody] UpdatePetRequest updateRequest)
         {
-            int res = _petRepository.Update(new Pet
+            #region
+            /*int res = _petRepository.Update(new Pet
             {
                 PetId = updateRequest.PetId,
                 ClientId = updateRequest.ClientId,
                 Name = updateRequest.Name,
                 Birthday = updateRequest.Birthday,
             });
-            return Ok(res);
+            return Ok(res);*/
+            #endregion
+            if (updateRequest.Birthday < DateTime.Now.AddYears(-25) || updateRequest.Name.Length < 3)
+            {
+                return BadRequest(0); // BadRequestObjectResult : 400
+            }
+            return Ok(1);
         }
 
         [HttpDelete("delete")]
         public ActionResult<int> Delete(int petId)
         {
-            int res = _petRepository.Delete(petId);
-            return Ok(res);
+            #region
+            /*int res = _petRepository.Delete(petId);
+            return Ok(res);*/
+            #endregion
+            if (petId <= 0)
+            {
+                return BadRequest(0);
+            }
+            return Ok(1);
         }
 
         [HttpGet("get-all")]
         public ActionResult<List<Pet>> GetAll()
         {
-            return Ok(_petRepository.GetAll());
+            //return Ok(_petRepository.GetAll());
+            return Ok(new List<Pet>());
         }
 
         [HttpGet("get-by-id")]
         public ActionResult<Pet> GetById(int petId)
         {
-            return Ok(_petRepository.GetById(petId));
+            //return Ok(_petRepository.GetById(petId));
+            if (petId <= 0)
+            {
+                return BadRequest(0);
+            }
+            return Ok(new Pet());
         }
     }
 }
---
+++ ClinicServiceTests/PetControllerTests.cs
@@ -0,0 +1,176 @@
+﻿using ClinicService.Controllers;
+using ClinicService.Models;
+using ClinicService.Models.Requests;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/// <summary>
+/// ДОМАШНЯЯ РАБОТА: разработать методы тестирования контроллера PetController:
+/// Обновление данных UpdateTest
+/// Получение данных (по всем животным) GetAllTest
+/// Получение данных (по конкретному животному) GetByIdTest
+/// </summary>
+
+
+namespace ClinicServiceTests
+{
+    public class PetControllerTests
+    {
+        private PetController _petController;
+
+        public PetControllerTests() 
+        {
+            _petController = new PetController();
+        }
+
+        [Fact]
+        public void PetCreateBadRequestTest()
+        {
+
+            CreatePetRequest createPetRequest = new CreatePetRequest();
+            createPetRequest.Name = "П";
+            createPetRequest.Birthday = DateTime.Now.AddYears(-55);
+            createPetRequest.ClientId = 1;
+
+            ActionResult<int> operationResult = _petController.Create(createPetRequest);
+
+            int expectedOperationValue = 0;
+
+            Assert.IsType<BadRequestObjectResult>(operationResult.Result);
+            Assert.Equal<int>(expectedOperationValue, (int)((BadRequestObjectResult)operationResult.Result).Value);
+
+        }
+
+        [Fact]
+        public void PetCreateTest()
+        {
+            // МЕТОД ТЕСТИРОВАНИЯ СОСТОИТ ИЗ УСЛОВНЫХ 3 ЧАСТЕЙ
+
+            // [1] Подготовка данных для тестирования
+            CreatePetRequest createPetRequest = new CreatePetRequest();
+            createPetRequest.Name = "Персик";
+            createPetRequest.Birthday = DateTime.Now.AddYears(-15);
+            createPetRequest.ClientId = 1;
+
+            // [2] Исполнение тестируемого метода
+            ActionResult<int> operationResult = _petController.Create(createPetRequest);
+
+
+            // [3] Подготовка эталонного результата (expected), проверка результата
+            int expectedOperationValue = 1;
+
+            Assert.IsType<OkObjectResult>(operationResult.Result);
+            Assert.Equal<int>(expectedOperationValue, (int)((OkObjectResult)operationResult.Result).Value);
+
+        }
+
+        [Fact]
+        public void PetUpdateBadRequestTest()
+        {
+            UpdatePetRequest updatePetRequest = new UpdatePetRequest();
+            updatePetRequest.Name = "То";
+            updatePetRequest.Birthday = DateTime.Now.AddDays(-100);
+            updatePetRequest.ClientId = 2;
+
+            ActionResult<int> operationResult = _petController.Update(updatePetRequest);
+
+            int expectedOperationValue = 0;
+
+            Assert.IsType<BadRequestObjectResult>(operationResult.Result);
+            Assert.Equal<int>(expectedOperationValue, (int)((BadRequestObjectResult)(operationResult.Result)).Value);
+
+        }
+
+       [Fact]
+        public void PetUpdateTest()
+        {
+            UpdatePetRequest updatePetRequest = new UpdatePetRequest();
+            updatePetRequest.Name = "Тошка";
+            updatePetRequest.Birthday = DateTime.Now.AddDays(-10);
+            updatePetRequest.ClientId = 2;
+
+            ActionResult<int> operationResult = _petController.Update(updatePetRequest);
+
+            int expectedOperationValue = 1;
+            Assert.IsType<OkObjectResult> (operationResult.Result);
+            Assert.Equal<int>(expectedOperationValue, (int)((OkObjectResult)operationResult.Result).Value);
+        }
+
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(-50)]
+        public void DeletePetBadRequestTest(int petId)
+        {
+
+            // [2] Исполнение тестируемого метода
+            ActionResult<int> operationResult = _petController.Delete(petId);
+
+            // [3] Подготовка эталонного результата (expected), проверка результата
+            int expectedOperationValue = 0;
+
+            Assert.IsType<BadRequestObjectResult>(operationResult.Result);
+            Assert.Equal<int>(expectedOperationValue, (int)((BadRequestObjectResult)operationResult.Result).Value);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(50)]
+        [InlineData(100)]
+        public void DeletePetTest(int petId)
+        {
+
+            // [2] Исполнение тестируемого метода
+            ActionResult<int> operationResult = _petController.Delete(petId);
+
+            // [3] Подготовка эталонного результата (expected), проверка результата
+            int expectedOperationValue = 1;
+
+            Assert.IsType<OkObjectResult>(operationResult.Result);
+            Assert.Equal<int>(expectedOperationValue, (int)((OkObjectResult)operationResult.Result).Value);
+        }
+
+        [Fact]
+        public void GetAllPetsTest()
+        {
+            ActionResult<List<Pet>> operationResult = _petController.GetAll();
+
+            Assert.IsType<OkObjectResult>(operationResult.Result);
+            Assert.IsAssignableFrom<List<Pet>>(((OkObjectResult)operationResult.Result).Value);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(50)]
+        [InlineData(100)]
+        public void GetByIdPetTest(int petId)
+        {
+            ActionResult<Pet> operationResult = _petController.GetById(petId);
+
+            Assert.IsType<OkObjectResult>(operationResult.Result);
+            Assert.IsAssignableFrom<Pet>(((OkObjectResult)operationResult.Result).Value);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(-100)]
+        public void GetByIdPetBadRequestTest(int petId)
+        {
+            ActionResult<Pet> operationResult = _petController.GetById(petId);
+
+            int expectedOperationValue = 0;
+
+            Assert.IsType<BadRequestObjectResult>(operationResult.Result);
+            Assert.Equal<int>(expectedOperationValue, (int)((BadRequestObjectResult)operationResult.Result).Value);
+        }
+    }
+
+    
+}
